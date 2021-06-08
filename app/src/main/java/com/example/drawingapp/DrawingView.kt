c8f6135c85208144f8d649ecbc189e85cd59fe8f
parangatp014 <seema.r@parangat.com>
2021-06-08 11:24:16 +0530

    Initial commit

---
+++ app/src/main/java/com/example/drawingapp/DrawingView.kt
@@ -0,0 +1,11 @@
+package com.example.drawingapp
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import java.util.jar.Attributes
+
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+}
